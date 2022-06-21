9565123f4d58bc80c527f63b717c0ef6475f64be
Ekjot Singh (Joe) <ekjot.singh@bookdepot.com>
2022-06-21 19:25:49 -0400

    innitial

---
+++ layout/footer/index.jsx
@@ -0,0 +1,9 @@
+import { Stack } from "@mui/material";
+import styles from "./Footer.module.css";
+export default function Footer() {
+  return (
+    <Stack mt={5} className={styles.main}>
+      Footer
+    </Stack>
+  );
+}
