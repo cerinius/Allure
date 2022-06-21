f364f115893fd57e9d9ede39b1c190eddf58f0bb
Ekjot Singh (Joe) <ekjot.singh@bookdepot.com>
2022-06-21 19:23:29 -0400

    innitial

--- layout/index.jsx
+++ layout/index.jsx
@@ -1,7 +1,22 @@
-export default function Layout(){
-    return (
-        <div>
-            Nav Menu
-        </div>
-    )
+import { Container, Divider, Stack } from "@mui/material";
+import Header from "./header";
+import Footer from "./footer";
+import styles from "./Layout.module.css";
+import { useEffect, useState } from "react";
+
+export default function Layout({ children }) {
+  return (
+    <Stack
+      className={styles.page}
+      spacing={2}
+      direction={"column"}
+      divider={<Divider orientation="horizontal" flexItem />}
+    >
+      <Header className={styles.header} />
+      <Stack className={styles.main}>
+        <Container>{children}</Container>
+      </Stack>
+      <Footer />
+    </Stack>
+  );
 }
